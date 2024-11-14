fe532de0a7b048f62b4656c8ade760940887129c
Quivir Cutanda <quivircutandaanora@gmail.com>
2024-11-14 10:46:02 +0800

    Added delete functionality and completed basic styling

--- db.php
+++ db.php
@@ -1,7 +1,30 @@
 <?php
-$conn = new mysqli('localhost', 'username', 'password', 'todo_app');
+$host = "localhost";
+$userName = "root";
+$password = "";
+$dbname = "todo_app";
+
+$conn = new mysqli($host, $userName, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname;";
+if(!mysqli_query($conn, $sql)){
+    die("");
+    $conn->close();
+}
+
+$conn->select_db($dbname);
+
+//create Table
+$sql = "CREATE TABLE  IF NOT EXISTS `tasks` (
+  `id` int(11) NOT NULL,
+  `task` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+);";
+
+if(!mysqli_query($conn, $sql)){
+    die("");
+}
